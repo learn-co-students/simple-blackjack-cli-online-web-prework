68ef5af811641d720867ab9cc9183a0842270cb2
Brandon Teich <brandonteich123@gmail.com>
2018-10-07 03:44:55 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -47,9 +47,6 @@ def hit?(number)
   end
 end
 
-def invalid_command
-  puts "Please enter a valid command"
-end
 
 #####################################################
 # get every test to pass before coding runner below #
