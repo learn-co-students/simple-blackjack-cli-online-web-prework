3e613a5acaabb0b37af79ed2bfe39c4f5ee7db2d
beaulieudev <beaulieudev@gmail.com>
2018-10-10 02:23:02 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,7 +7,7 @@ def deal_card
 end
 
 def display_card_total(card_total)
-  puts "Your cards add up to #{card_total}".
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
@@ -18,18 +18,17 @@ def get_user_input
   gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
   card_total = deal_card + deal_card
   display_card_total(card_total)
+  return card_total
 end
 
 def hit?(card_total)
-  # code hit? here
   prompt_user
   input = get_user_input
   if input == "s"
@@ -57,6 +56,6 @@ def runner
   until card_total > 21
    card_total = hit?(card_total)
    display_card_total(card_total)
- end
-end_game(card_total)
+  end
+  end_game(card_total)
 end
