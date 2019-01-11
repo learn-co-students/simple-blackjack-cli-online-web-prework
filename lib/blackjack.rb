6fbf2b87925118e43238231c29703b3b263c2e4d
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-11 05:13:02 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,37 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table".
 end
 
 def deal_card
-  # code #deal_card here
+  puts "1-11"
 end
 
 def display_card_total
-  # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h'to hit or 's'to stay".
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets
 end
 
 def end_game
-  # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  puts #display_card_total
 end
 
 def hit?
-  # code hit? here
+  puts #prompt_user
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "25"
 end
 
 #####################################################
@@ -39,6 +39,5 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  puts #runner
 end
-    
