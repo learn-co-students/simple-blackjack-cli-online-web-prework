00b52c2882e72bdce2e7bf8881b0a17761f61992
Nash Brancic <nashfft@gmail.com>
2019-01-27 00:32:13 +0000

    Done.

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,10 @@
 require_relative "blackjack.rb"
-
-runner
+def runner
+  welcome
+  hand=initial_round
+  until hand>21 do
+    hand=hit?(hand)
+    display_card_total(hand)
+  end
+  end_game(hand)
+end
