b72b38fdc12a2940bfc901e16024bfb8ba9acc8d
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-03 21:02:42 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,68 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table" # code #welcome here
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)# code #deal_card here
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}" # code #display_card_total here
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay" # code #prompt_user here
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp # code #get_user_input here
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!" # code #end_game here
 end
 
+# code #initial_round here
 def initial_round
-  # code #initial_round here
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total(card_total)
+  return card_total
 end
 
-def hit?
-  # code hit? here
+initial_round
+
+# code hit? here
+def hit?(card_total)
+  prompt_user
+  user_input = get_user_input 
+  case user_input 
+    when 's'
+      card_total
+    when 'h'
+      card_total += deal_card
+    else invalid_command
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command" # code invalid_command here
 end
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
+# code runner here
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21 
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
     
+runner
