554e5571fe95cdd41478da69df46cdbf568105b9
Charlotte Neff <charlotte.neff@gmail.com>
2019-02-22 18:33:15 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -16,23 +16,38 @@ def prompt_user
 end
 
 def get_user_input
-  gets.chomp # code #get_user_input here
+  gets.chomp.strip # code #get_user_input here
 end
 
 def end_game
-  # code #end_game here
+  puts "Sorry, you hit #{card total}. Thanks for playing!" # code #end_game here
 end
 
 def initial_round
+  first_round = deal_card + deal_card
+  display_card_total(first_round) 
+  return first_round
   # code #initial_round here
 end
 
-def hit?
+def hit?(card_total)
+  prompt_user
+  input = get_user_input
+  until input == 'h' || input == 's'
+  invalid_command
+  prompt_user
+  input = get_user_input
+end
+if input == 'h'
+  card_total += deal_card
+  elsif input == 's'
+  card_total
+end
   # code hit? here
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"# code invalid_command here
 end
 
 #####################################################
@@ -40,6 +55,14 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+  card_total = hit?(card_total)
+  display_card_total(card_total)
+  end
+  end_game(card_total)
+  end
+# code runner here
 end
     
