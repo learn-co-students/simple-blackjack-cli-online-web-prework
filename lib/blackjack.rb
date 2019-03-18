852f15815dcb40f5ac6ec24d5fbf73188873629e
Craig Donavin <crdonavin@gmail.com>
2019-03-18 03:49:24 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,10 +49,9 @@ end
 
 def runner
   welcome
-  initial_round
   card_num = initial_round
   until card_num >= 21
-    card_num += hit?(get_user_input)
+    card_num = hit?(card_num)
     display_card_total(card_num)
   end
 end_game(card_num)
