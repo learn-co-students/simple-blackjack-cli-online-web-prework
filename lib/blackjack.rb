3482f11c93d39be758e7aafeae7a814cb7e30100
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-26 13:47:43 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,85 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  card = rand(1..11)
+  return card
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
+  answer = gets.chomp
+  return answer
   # code #get_user_input here
 end
 
-def end_game
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!" 
   # code #end_game here
 end
 
 def initial_round
-  # code #initial_round here
+  count = 0
+  while count < 2
+  card = deal_card
+  card += card
+  count += 1
 end
-
-def hit?
-  # code hit? here
+display_card_total(card)
+return card
+# code #initial_round here
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
+
+def hit?(hitme)
+prompt_user
+answer = get_user_input
+if answer == 's'
+card = initial_round
+return card
+elsif answer == 'h'
+new = deal_card
+card = hitme + new
+return card
+else
+invalid_command
+prompt_user
+end
+display_card_total(card)
+end 
+
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+ welcome
+card = 0
+until card > 21
+newcard = hit?(card)
+card += newcard
+display_card_total(card)
+end 
+return card
+ 
+return end_game
 end
     
