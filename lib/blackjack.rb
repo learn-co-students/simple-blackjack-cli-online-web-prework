129ceee3225ff6e77b3a271f78f6e0059451940d
voluble-bug-0219 <montensia1@outlook.com>
2019-05-13 23:38:24 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,60 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
+def display_card_total num
   # code #display_card_total here
+  puts "Your cards add up to #{num}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  user_input = gets.chomp
 end
 
-def end_game
+def end_game card_total
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!" 
 end
 
 def initial_round
   # code #initial_round here
+  sum = 0
+  2.times do
+    sum += deal_card
+  end
+  display_card_total sum
+  return sum
 end
 
-def hit?
+def hit? num
   # code hit? here
+  prompt_user
+  user_input = get_user_input  
+  if user_input == 'h'
+    num += deal_card
+  elsif user_input == 's'
+   num
+  else
+    invalid_command
+  end
+  return num
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -40,5 +63,12 @@ end
 
 def runner
   # code runner here
+  welcome 
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit? card_total
+    display_card_total card_total
+  end
+  end_game card_total
 end
     
