cf1a8eaabcd52dc19b896c7cf593ffd2bdf200ad
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-24 19:22:21 +0000

    Done.

--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -1,3 +1,7 @@
+require_relative "./spec_helper.rb"
+require_relative "../lib/blackjack.rb"
+
+
 describe "#welcome" do
   it "prints a welcome message to screen" do
     expect($stdout).to receive(:puts).with("Welcome to the Blackjack Table")
